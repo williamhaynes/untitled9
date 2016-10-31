89a486ab6a62cef23cfb6b66eeb9745ebc940fb6
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-31 15:47:37 +0000

    just do it

--- inc/scripts/dbconnect.php
+++ inc/scripts/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'servername');
-define('DB_USERNAME', 'username');
-define('DB_PASSWORD', 'password');
-define('DB_DATABASE', 'database');
+define('DB_SERVER', 'us-cdbr-azure-southcentral-f.cloudapp.net');
+define('DB_USERNAME', 'b21eaed643e4cb');
+define('DB_PASSWORD', '4756773b');
+define('DB_DATABASE', 'wjh0001db');
 $db = mysqli_connect(DB_SERVER,
     DB_USERNAME, DB_PASSWORD,
     DB_DATABASE);
