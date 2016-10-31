1b2423863c8be1d6f7c5f7e4be575a5e014a9cb0
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-31 15:45:26 +0000

    just do it

--- inc/scripts/dbconnect.php
+++ inc/scripts/dbconnect.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hype_
- * Date: 31/10/2016
- * Time: 14:13
- */
+define('DB_SERVER', 'servername');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'database');
+$db = mysqli_connect(DB_SERVER,
+    DB_USERNAME, DB_PASSWORD,
+    DB_DATABASE);
+?>
